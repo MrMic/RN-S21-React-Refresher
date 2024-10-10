fc528c270594d508ce1ddcf076ceadca4eece602
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-10-10 15:05:52 +0200

    feat: Work w/ Props

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import './App.css';
 import GoalList from './components/GoalList';
 
 function App() {
+  const courseGoal = [
+    { id: 'cg1', text: 'Finish the course' },
+    { id: 'cg2', text: 'Learn all about the course main topics' },
+    { id: 'cg3', text: 'Help Other students' },
+  ];
+
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <GoalList />
+      <GoalList goals={courseGoal} />
     </div >
   );
 }
