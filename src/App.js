49f0e95bc9587a4120ee9fcbd1a9d80ecb480763
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-10-12 14:40:47 +0200

    feat(UX): Parent-Child communication

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import GoalList from './components/GoalList/GoalList';
 import NewGoal from './components/NewGoal/NewGoal';
 
 function App() {
-  const courseGoal = [
+  const courseGoals = [
     { id: 'cg1', text: 'Finish the course' },
     { id: 'cg2', text: 'Learn all about the course main topics' },
     { id: 'cg3', text: 'Help Other students' },
   ];
 
+  function addGoalHandler(newGoal) {
+    courseGoals.push(newGoal);
+    console.log("🪚 courseGoals:", courseGoals);
+  }
+
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <NewGoal />
-      <GoalList goals={courseGoal} />
+      <NewGoal onAddGoal={addGoalHandler} />
+      <GoalList goals={courseGoals} />
     </div >
   );
 }
