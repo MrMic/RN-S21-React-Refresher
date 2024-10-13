9bd53060252963303ca7ed1c52391598e899d95d
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-10-13 11:23:30 +0200

    feat: User Input & 2-Way binding

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -1,23 +1,41 @@
+import { useState } from 'react';
 import './NewGoal.css'
 function NewGoal(props) {
+  const [enteredText, setEnteredText] = useState('');
+
   // ______________________________________________________________________
   function addGoalHandler(event) {
     event.preventDefault();
 
+    if (enteredText.trim().length === 0) {
+      return;
+    }
+
     const newGoal = {
       id: Math.random().toString(),
-      text: 'My new Goal!',
+      text: enteredText,
     };
+
+    setEnteredText('');
 
     props.onAddGoal(newGoal);
 
   }
 
   // ______________________________________________________________________
+  function textChangeHandler(event) {
+    setEnteredText(event.target.value);
+  }
+
+  // ______________________________________________________________________
   return (
     <div className="new-goal">
       <form onSubmit={addGoalHandler}>
-        <input type="text" />
+        <input
+          type="text"
+          value={enteredText}
+          onChange={textChangeHandler}
+        />
         <button type="submit">Add Goal</button>
       </form>
     </div>
